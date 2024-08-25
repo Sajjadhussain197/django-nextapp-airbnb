172867fcf4ab71188b789c49f2a1a8a812b544f1
sajjadhussain197 <sajjadhussain9582@gmail.com>
2024-08-25 08:24:41 +0500

    add property is expanded

--- app/components/forms/CustomButton.tsx
+++ app/components/forms/CustomButton.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
-interface CustomButtonProps{
-  label:string;
-  onClick: ()=>void
-}
-const CustomButton:React.FC<CustomButtonProps> = ({
-  label, onClick, 
-}) => {
-  return (
-    <div className='w-full py-4 bg-airbnb hover:bg-airbnbdark text-white cursor-pointer rounded-xl transition text-center lg:w-[100px]'
-    onClick={onClick}>
-      {label}
-    </div>
-  )
+interface CustomButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string; // Optional className prop
 }
 
-export default CustomButton
+const CustomButton: React.FC<CustomButtonProps> = ({
+  label, onClick, className,
+}) => {
+  return (
+    <div 
+      className={`w-full py-4 bg-airbnb hover:bg-airbnbdark text-white cursor-pointer rounded-xl transition text-center lg:w-[100px] ${className}`} 
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+}
+
+export default CustomButton;
--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -12,6 +12,12 @@ const AddPropertyModal = () => {
     const [currentStep, setCurrentStep] = useState(1);
     const addPropertyModal = useAddPropertyModal();
     const [dataCategory, setDataCategory] = useState('');
+    const [dataTitle, setDataTitle] = useState('')
+    const [dataDescription, setDataDescription] = useState('')
+    const [dataPrice, setDataPrice] = useState('');
+    const [dataBedrooms, setDataBedrooms] = useState('');
+    const [dataBathrooms, setDataBathrooms] = useState('');
+    const [dataGustes, setDataGustes] = useState('')
 
     
 
@@ -23,14 +29,58 @@ const AddPropertyModal = () => {
     const content = (
         <>
         {currentStep === 1 ? (
-            <><h2>Choose Category</h2>
+            <>
+            
+            <h2 className='mb-6 text-2xl '>Choose Category</h2>
             <Categories
             dataCategory={dataCategory}
             setCategory={(category)=>setCategory(category)}/>
         <CustomButton label='next' onClick={()=>setCurrentStep(2)} />
             </>
+        ): currentStep == 2 ?(<>
+
+            <h2 className='mb-6 text-2xl '>Describe your place</h2>
+            <div className="pt-3 pb-6 space-y-4">
+                <div className="flex flex-col space-y-2">
+                    <label htmlFor="">Title</label>
+                    <input type="text"
+                    value={dataTitle}
+                    onChange={(e)=>setDataTitle(e.target.value)}
+                    className='w-full p-4 border-gray-600 rounded-xl' />
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <label htmlFor="">Description</label>
+                    <textarea
+                    
+                    value={dataDescription}
+                    onChange={(e)=>setDataDescription(e.target.value)}
+                    className='w-full p-4 border-gray-600 h-[200px] rounded-xl' />
+                </div>
+            </div>
+            <CustomButton label='Previouse' 
+            className='mb-2 bg-black hover:bg-gray-800' 
+            onClick={()=>setCurrentStep(1)} />
+            <CustomButton label='Next' onClick={()=>setCurrentStep(3)} />
+        
+        </>
         ):(
-            <p>step 2</p>
+            <>
+            
+            <h2 className='mb-6 text-2xl '>Details</h2>
+            <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+                    <label htmlFor="">Price Per night</label>
+                    <input type="number"
+                    value={dataPrice}
+                    onChange={(e)=>setDataPrice(e.target.value)}
+                    className='w-full p-4 border-gray-600 rounded-xl' />
+                </div>
+                </div>
+              <CustomButton label='Previouse' 
+            className='mb-2 bg-black hover:bg-gray-800' 
+            onClick={()=>setCurrentStep(1)} />
+            <CustomButton label='Next' onClick={()=>setCurrentStep(3)} />
+            </>
         )}
         
         </>
